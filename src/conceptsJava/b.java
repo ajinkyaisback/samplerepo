2c9e5d13b04d04c7ea4e6e48853ef62be3740379
ajinkyaisback <50210672+ajinkyaisback@users.noreply.github.com>
2023-02-13 22:42:39 +0530

    Update b.java

--- src/conceptsJava/b.java
+++ src/conceptsJava/b.java
@@ -3,10 +3,10 @@ package conceptsJava;
 public class b {
 
 	public static void main(String[] args) {
-System.out.println("dev1 code");
-System.out.println("dev1 code");
-System.out.println("dev1 code");
-System.out.println("dev1 code");
+System.out.println("dev2 code");
+System.out.println("dev2 code");
+System.out.println("dev2 code");
+System.out.println("dev2 code");
 
 	}
 
