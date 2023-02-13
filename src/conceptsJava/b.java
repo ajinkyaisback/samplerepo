5c36c54ac2a1eed14b350786b4dee952d131e83b
ajinkyaisback <50210672+ajinkyaisback@users.noreply.github.com>
2023-02-13 22:42:09 +0530

    Create b.java

---
+++ src/conceptsJava/b.java
@@ -0,0 +1,13 @@
+package conceptsJava;
+
+public class b {
+
+	public static void main(String[] args) {
+System.out.println("dev1 code");
+System.out.println("dev1 code");
+System.out.println("dev1 code");
+System.out.println("dev1 code");
+
+	}
+
+}
