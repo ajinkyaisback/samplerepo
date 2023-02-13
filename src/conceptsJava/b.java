832da6045b917ad7009a2719d9a4c351abc34702
ajinkyaisback <50210672+ajinkyaisback@users.noreply.github.com>
2023-02-13 22:48:58 +0530

    Create b.java

---
+++ src/conceptsJava/b.java
@@ -0,0 +1,12 @@
+package conceptsJava;
+
+public class b {
+
+	public static void main(String[] args) {
+ System.out.println("welcome coder 2");
+ System.out.println("welcome coder 2");
+ System.out.println("welcome coder 2");
+ System.out.println("welcome coder 2");
+	}
+
+}
