b3aa9cc44f810febecb6ed51c35254aa72c98af5
ajinkyaisback <ajinkyagadkari100@gmail.com>
2023-02-13 22:17:33 +0530

    dev 1 Code added after checking with dev2

--- src/conceptsJava/a.java
+++ src/conceptsJava/a.java
@@ -3,8 +3,13 @@ package conceptsJava;
 public class a {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		String Dev2Code="Dev 2 Code";
 		System.out.println(Dev2Code);
+=======
+		String Dev1Code="Dev 1 Code";
+		System.out.println(Dev1Code);
+>>>>>>> Stashed changes
 
 	}
 
